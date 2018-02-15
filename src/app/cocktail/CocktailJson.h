95253edd4eec365f0c5ff4b151269bb9d28e649f
mras2an <aqzsedrftgy18@gmail.com>
2018-02-15 20:57:43 +0100

    project: add bottles and update measure managment.

--- src/app/cocktail/CocktailJson.h
+++ src/app/cocktail/CocktailJson.h
@@ -4,13 +4,16 @@
 char bb[] =
   "{\"bottles\":[{\"bottle\":{\"name\":\"eau\",\"note\":\"0%/vol\",\"position\":0}},"
   "{\"bottle\":{\"name\":\"whisky\",\"note\":\"50%/vol\",\"position\":1}},"
-  "{\"bottle\":{\"name\":\"tequila\",\"note\":\"50%/vol\",\"position\":2}},"
+  //"{\"bottle\":{\"name\":\"tequila\",\"note\":\"50%/vol\",\"position\":2}},"
+  "{\"bottle\":{\"name\":\"rhum blanc\",\"note\":\"50%/vol\",\"position\":2}},"
   "{\"bottle\":{\"name\":\"pastis\",\"note\":\"50%/vol\",\"position\":3}},"
   //"{\"bottle\":{\"name\":\"sirop de sucre\",\"note\":\"50%/vol\",\"position\":4}},"
-  "{\"bottle\":{\"name\":\"get 27\",\"note\":\"0%/vol\",\"position\":4}},"
+  //"{\"bottle\":{\"name\":\"get 27\",\"note\":\"0%/vol\",\"position\":4}},"
+  "{\"bottle\":{\"name\":\"vodka\",\"note\":\"0%/vol\",\"position\":4}},"
   "{\"bottle\":{\"name\":\"curacao bleu\",\"note\":\"20%/vol\",\"position\":5}},"
   "{\"bottle\":{\"name\":\"cointreau\",\"note\":\"25%/vol\",\"position\":6}},"
-  "{\"bottle\":{\"name\":\"rhum blanc\",\"note\":\"50%/vol\",\"position\":7}}]}";
+  "{\"bottle\":{\"name\":\"get 27\",\"note\":\"50%/vol\",\"position\":7}}]}";
+  //"{\"bottle\":{\"name\":\"rhum blanc\",\"note\":\"50%/vol\",\"position\":7}}]}";
 
 char cc[] =
   "{\"cocktails\":[{\"cocktail\":{\"name\":\"Mojito\",\"ingredients\":[{\"ingredient\":{\"name\":\"rhum blanc\",\"measure\":2}},{\"ingredient\":{\"name\":\"menthe verte\",\"measure\":1}},{\"ingredient\":{\"name\":\"jus de citron\",\"measure\":1}},{\"ingredient\":{\"name\":\"sucre de canne\",\"measure\":1}},{\"ingredient\":{\"name\":\"eau gazeuse\",\"measure\":2}},{\"ingredient\":{\"name\":\"glace pilee\",\"measure\":1}}]}},"
@@ -20,7 +23,7 @@ char cc[] =
   "{\"cocktail\":{\"name\":\"JagerBomb\",\"ingredients\":[{\"ingredient\":{\"name\":\"jagermeister\",\"measure\":1}},{\"ingredient\":{\"name\":\"redbull\",\"measure\":2}}]}},"
   "{\"cocktail\":{\"name\":\"BlueSea\",\"ingredients\":[{\"ingredient\":{\"name\":\"limonade\",\"measure\":1}},{\"ingredient\":{\"name\":\"jus d'un citrons\",\"measure\":1}},{\"ingredient\":{\"name\":\"rhum blanc\",\"measure\":1}},{\"ingredient\":{\"name\":\"curacao bleu\",\"measure\":1}},{\"ingredient\":{\"name\":\"cointreau\",\"measure\":1}},{\"ingredient\":{\"name\":\"tequila\",\"measure\":1}}]}},"
   "{\"cocktail\":{\"name\":\"Cascade\",\"ingredients\":[{\"ingredient\":{\"name\":\"jus d'orange\",\"measure\":2}},{\"ingredient\":{\"name\":\"curacao bleu\",\"measure\":1}},{\"ingredient\":{\"name\":\"get 27\",\"measure\":1}},{\"ingredient\":{\"name\":\"rhum blanc\",\"measure\":2}},{\"ingredient\":{\"name\":\"curacao bleu\",\"measure\":1}}]}},"
-  "{\"cocktail\":{\"name\":\"PetitJaune\",\"noteF\":\"pastis, eau.\",\"noteA\":\"\",\"ingredients\":[{\"ingredient\":{\"name\":\"pastis\",\"measure\":1}},{\"ingredient\":{\"name\":\"eau\",\"measure\":5}}]}},"
+  "{\"cocktail\":{\"name\":\"PetitJaune\",\"noteF\":\"pastis, eau.\",\"noteA\":\"\",\"ingredients\":[{\"ingredient\":{\"name\":\"pastis\",\"measure\":2}},{\"ingredient\":{\"name\":\"eau\",\"measure\":5}}]}},"
   "{\"cocktail\":{\"name\":\"Daiquiri\",\"ingredients\":[{\"ingredient\":{\"name\":\"rhum blanc\",\"measure\":2}},{\"ingredient\":{\"name\":\"sirop de sucre\",\"measure\":1}},{\"ingredient\":{\"name\":\"jus de citron\",\"measure\":2}}]}},"
   "{\"cocktail\":{\"name\":\"Perroget\",\"ingredients\":[{\"ingredient\":{\"name\":\"pastis\",\"measure\":1}},{\"ingredient\":{\"name\":\"get 27\",\"measure\":1}},{\"ingredient\":{\"name\":\"eau\",\"measure\":2}}]}},"
   "{\"cocktail\":{\"name\":\"Goudron\",\"ingredients\":[{\"ingredient\":{\"name\":\"pastis\",\"measure\":1}},{\"ingredient\":{\"name\":\"coca\",\"measure\":2}}]}},"
--- src/common/hardware/component/motor/MotorHandling.c
+++ src/common/hardware/component/motor/MotorHandling.c
@@ -70,11 +70,47 @@ void MotorHandling_setPositionOnX(int position)
 }
 
 /******************************************************************************
- * FunctionName : static void MotorHandling_get2ml()
+ * FunctionName : static void MotorHandling_get10ml()
  * Parameters   : none
  * Returns      : none
  *******************************************************************************/
-static void MotorHandling_get2ml()
+static void MotorHandling_get10ml()
+{
+  int end = MOTOR_LIMIT;
+  BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_DIR, BAR_LEVEL_LOW));
+
+  for(int i = 0; i < end; i++)
+  {
+    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_LOW));
+    CpuDelay_ms(1);
+    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_HIGH));
+    CpuDelay_ms(1);
+  }
+
+  CpuDelay_ms(800);
+  uint32_t bp2[1];
+  Gpio_get(DETECTION_AXE_Y, bp2);
+  BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_DIR, BAR_LEVEL_HIGH));
+  end = MOTOR_LIMIT;
+
+  while(bp2[0] && (end != 0))
+  {
+    Gpio_get(DETECTION_AXE_Y, bp2);
+    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_LOW));
+    CpuDelay_ms(1);
+    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_HIGH));
+    CpuDelay_ms(1);
+    end--;
+  }
+  CpuDelay_ms(800);
+}
+
+/******************************************************************************
+ * FunctionName : static void MotorHandling_get25ml()
+ * Parameters   : void
+ * Returns      : none
+ *******************************************************************************/
+static void MotorHandling_get25ml()
 {
   int end = MOTOR_LIMIT;
   BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_DIR, BAR_LEVEL_LOW));
@@ -102,44 +138,9 @@ static void MotorHandling_get2ml()
     CpuDelay_ms(1);
     end--;
   }
-}
 
-/******************************************************************************
- * FunctionName : static void MotorHandling_get4ml()
- * Parameters   : void
- * Returns      : none
- *******************************************************************************/
-//static void MotorHandling_get4ml()
-//{
-//  int end = MOTOR_LIMIT;
-//  BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_DIR, BAR_LEVEL_LOW));
-//
-//  for(int i = 0; i < end; i++)
-//  {
-//    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_LOW));
-//    CpuDelay_ms(1);
-//    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_HIGH));
-//    CpuDelay_ms(1);
-//  }
-//
-//  CpuDelay_ms(3000);
-//  uint32_t bp2[1];
-//  Gpio_get(DETECTION_AXE_Y, bp2);
-//  BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_DIR, BAR_LEVEL_HIGH));
-//  end = MOTOR_LIMIT;
-//
-//  while(bp2[0] && (end != 0))
-//  {
-//    Gpio_get(DETECTION_AXE_Y, bp2);
-//    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_LOW));
-//    CpuDelay_ms(1);
-//    BAR_ERROR_CHECK(Gpio_set(MOTOR_AXE_Y_CLK, BAR_LEVEL_HIGH));
-//    CpuDelay_ms(1);
-//    end--;
-//  }
-//
-//  CpuDelay_ms(3000);
-//}
+  CpuDelay_ms(800);
+}
 
 /******************************************************************************
  * FunctionName : void MotorHandling_getAMeasureOnY(int measure)
@@ -150,39 +151,35 @@ void MotorHandling_getAMeasureOnY(int measure)
 {
   BarDebug_info("\t\t MotorHandling_getAMeasureOnY \n");
 
-  for(int i = 0; i < measure; i++)
+  if(measure == 1)
   {
-	  MotorHandling_get2ml();
+    MotorHandling_get10ml();
   }
-  CpuDelay_ms(500);
-//  if(measure == 1)
-//  {
-//    MotorHandling_get2ml();
-//  }
-//
-//  if(measure == 2)
-//  {
-//    MotorHandling_get4ml();
-//  }
-//
-//  if(measure == 3)
-//  {
-//    MotorHandling_get4ml();
-//    MotorHandling_get2ml();
-//  }
-//
-//  if(measure == 4)
-//  {
-//    MotorHandling_get4ml();
-//    MotorHandling_get4ml();
-//  }
-//
-//  if(measure == 5)
-//  {
-//    MotorHandling_get4ml();
-//    MotorHandling_get4ml();
-//    MotorHandling_get2ml();
-//  }
+
+  if(measure == 2)
+  {
+    MotorHandling_get25ml();
+  }
+
+  if(measure == 3)
+  {
+    MotorHandling_get25ml();
+    MotorHandling_get10ml();
+  }
+
+  if(measure == 4)
+  {
+    MotorHandling_get25ml();
+    MotorHandling_get25ml();
+  }
+
+  if(measure == 5)
+  {
+    MotorHandling_get25ml();
+    MotorHandling_get25ml();
+    MotorHandling_get10ml();
+  }
+
 }
 
 /******************************************************************************
@@ -199,7 +196,7 @@ void MotorHandling_getAMeasureOnPump(int measure)
 
   for(int j = 0; j < measure; j++)
   {
-    CpuDelay_ms(600);
+    CpuDelay_ms(550);
   }
 
   BAR_ERROR_CHECK(Gpio_set(MOTOR_PUMP_3, BAR_LEVEL_LOW));
